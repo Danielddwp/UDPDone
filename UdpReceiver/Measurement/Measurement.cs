08c50767e4d3c0d2cd2205b4b15ed706b277ffce
Danielddwp <48202417+Danielddwp@users.noreply.github.com>
2020-05-20 15:43:23 +0200

    Add files via upload

---
+++ UdpReceiver/Measurement/Measurement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Measurement
+{
+    public class Measurement
+    {
+        
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public double Pressure { get; set; }
+        
+       
+
+        public Measurement(int temperature, int humidity, double pressure)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Pressure = pressure;
+           
+            
+        }
+
+        public override string ToString()
+        {
+            
+            return Temperature + " " + Humidity + " " + Pressure ;
+        }
+    }
+}
---
+++ UdpReceiver/Measurement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Measurement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
